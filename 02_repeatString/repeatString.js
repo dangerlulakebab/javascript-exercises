8e070156bc4ada17bc4673e402c7d4449190b64c
kebab <darknoobssquad@gmail.com>
2024-08-26 20:15:46 +0300

    did 7 exercises/ 12

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
 
+    if (times < 0) {
+        return "ERROR";
+    }
+    let string = ''
+    for (num = 0; num < times; num++){
+        string += word
+    }
+    return string
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...objects) {
+let newArray = array.filter(item => !objects.includes(item))
+
+return newArray
 };
 
 // Do not edit below this line
